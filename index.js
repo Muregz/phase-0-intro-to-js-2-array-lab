e2cad4a12134ce4ba10b03e9f53444ac8e737ddb
Muregz <fabian.murega@student.moringaschool.com>
2024-07-03 10:11:41 +0300

    master

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    
+    let newCatsArray = [...cats, name];
+    return newCatsArray;
+}
+function prependCat(name) {
+    
+    let newCatsArray = [name, ...cats];
+    return newCatsArray;
+}
+function removeLastCat() {
+    
+    let newCatsArray = cats.slice(0, -1);
+    return newCatsArray;
+}
+function removeFirstCat() {
+   let newCatsArray = cats.slice(1);
+    return newCatsArray;
+}
+
+
+
